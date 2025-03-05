a02dc08b66cfb652d7ea3ac0a3a110ccc202b57c
QIUYU YANG <wwwyqy618@gmail.com>
2025-03-06 00:34:46 +0800

    retrain side model

--- custom_indicators/config.py
+++ custom_indicators/config.py
@@ -5,15 +5,15 @@ with open(Path(__file__).parent / "feature_info.json", "r") as f:
     feature_info = json.load(f)
 
 
-SIDE_3M = feature_info["side"]["3m"]
-SIDE_15M = feature_info["side"]["15m"]
-SIDE_1H = feature_info["side"]["1h"]
+SIDE_3M = feature_info["side"]["10m"]
+SIDE_15M = feature_info["side"]["45m"]
+SIDE_1H = feature_info["side"]["4h"]
 
 SIDE_ALL = SIDE_3M + SIDE_15M + SIDE_1H
 
-META_3M = feature_info["meta"]["3m"]
-META_15M = feature_info["meta"]["15m"]
-META_1H = feature_info["meta"]["1h"]
+META_3M = feature_info["meta"]["10m"]
+META_15M = feature_info["meta"]["45m"]
+META_1H = feature_info["meta"]["4h"]
 META_MODEL_RES = feature_info["meta"]["model_res"]
 
 META_ALL = META_3M + META_15M + META_1H + META_MODEL_RES
