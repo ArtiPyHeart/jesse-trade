cc1956cb1e22d956a1154682dc0638ecc8c9131c
QIUYU YANG <wwwyqy618@gmail.com>
2025-03-08 18:24:59 +0800

    25m db: select side features

--- custom_indicators/config.py
+++ custom_indicators/config.py
@@ -1,23 +1,23 @@
 import json
 from pathlib import Path
 
-with open(Path(__file__).parent / "feature_info.json", "r") as f:
-    feature_info = json.load(f)
+# with open(Path(__file__).parent / "feature_info.json", "r") as f:
+#     feature_info = json.load(f)
 
 
-SIDE_15M = feature_info["side"]["15m"]
-SIDE_45M = feature_info["side"]["45m"]
-SIDE_4H = feature_info["side"]["4h"]
+# SIDE_10M = feature_info["side"]["10m"]
+# SIDE_25M = feature_info["side"]["25m"]
+# SIDE_2H = feature_info["side"]["2h"]
 
-SIDE_ALL = SIDE_15M + SIDE_45M + SIDE_4H
+# SIDE_ALL = SIDE_10M + SIDE_25M + SIDE_2H
 
-META_15M = feature_info["meta"]["15m"]
-META_45M = feature_info["meta"]["45m"]
-META_4H = feature_info["meta"]["4h"]
-META_MODEL_RES = feature_info["meta"]["model_res"]
+# META_10M = feature_info["meta"]["10m"]
+# META_25M = feature_info["meta"]["25m"]
+# META_2H = feature_info["meta"]["2h"]
+# META_MODEL_RES = feature_info["meta"]["model_res"]
 
-META_ALL = META_15M + META_45M + META_4H + META_MODEL_RES
+# META_ALL = META_10M + META_25M + META_2H + META_MODEL_RES
 
-DOLLAR_BAR_THRESHOLD_15M = 115851580.74615964
-DOLLAR_BAR_THRESHOLD_45M = 379150625.6237953
-DOLLAR_BAR_THRESHOLD_4H = 2078307128.56747
+DOLLAR_BAR_THRESHOLD_10M = 71968406.59988706
+DOLLAR_BAR_THRESHOLD_25M = 203617929.03870487
+DOLLAR_BAR_THRESHOLD_2H = 1039153564.783735
