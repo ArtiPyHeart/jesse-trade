30fd105e89c2afe8a9ab16442e3f5c004610e7bc
QIUYU YANG <wwwyqy618@gmail.com>
2025-03-08 09:09:11 +0800

    checkpoint

--- custom_indicators/config.py
+++ custom_indicators/config.py
@@ -5,19 +5,19 @@ with open(Path(__file__).parent / "feature_info.json", "r") as f:
     feature_info = json.load(f)
 
 
-SIDE_3M = feature_info["side"]["10m"]
-SIDE_15M = feature_info["side"]["45m"]
-SIDE_1H = feature_info["side"]["4h"]
+SIDE_15M = feature_info["side"]["15m"]
+SIDE_45M = feature_info["side"]["45m"]
+SIDE_4H = feature_info["side"]["4h"]
 
-SIDE_ALL = SIDE_3M + SIDE_15M + SIDE_1H
+SIDE_ALL = SIDE_15M + SIDE_45M + SIDE_4H
 
-META_3M = feature_info["meta"]["10m"]
-META_15M = feature_info["meta"]["45m"]
-META_1H = feature_info["meta"]["4h"]
+META_15M = feature_info["meta"]["15m"]
+META_45M = feature_info["meta"]["45m"]
+META_4H = feature_info["meta"]["4h"]
 META_MODEL_RES = feature_info["meta"]["model_res"]
 
-META_ALL = META_3M + META_15M + META_1H + META_MODEL_RES
+META_ALL = META_15M + META_45M + META_4H + META_MODEL_RES
 
-DOLLAR_BAR_THRESHOLD_7M = 47392922.79565829
-DOLLAR_BAR_THRESHOLD_14M = 107072898.39019096
-DOLLAR_BAR_THRESHOLD_84M = 716159708.1343918
+DOLLAR_BAR_THRESHOLD_15M = 115851580.74615964
+DOLLAR_BAR_THRESHOLD_45M = 379150625.6237953
+DOLLAR_BAR_THRESHOLD_4H = 2078307128.56747
