75322c2ee63252f4b74023cffc71ad4c6db61b80
qiuyu.yang <qiuyu.yang@realai.ai>
2025-04-07 11:36:37 +0800

    checkpoint

--- strategies/BinanceMLV2/config.py
+++ strategies/BinanceMLV2/config.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 import lightgbm as lgb
@@ -14,8 +15,8 @@ DOLLAR_BAR_THRESHOLD_MID = 551319274.2985482
 DOLLAR_BAR_THRESHOLD_LONG = 1421823390.033098
 DOLLAR_BAR_THRESHOLD_SUPER_LONG = 3307915640.791289
 
-meta_model_path = Path(__file__).parent / "model_meta.txt"
-meta_model_prod_path = Path(__file__).parent / "model_meta_prod.txt"
+meta_model_path = Path(__file__).parent / "model" / "model_meta.txt"
+meta_model_prod_path = Path(__file__).parent / "model" / "model_meta_prod.txt"
 
 
 def get_meta_model(is_livetrading: bool):
@@ -27,8 +28,8 @@ def get_meta_model(is_livetrading: bool):
         return meta_model
 
 
-side_model_path = Path(__file__).parent / "model_side.txt"
-side_model_prod_path = Path(__file__).parent / "model_side_prod.txt"
+side_model_path = Path(__file__).parent / "model" / "model_side.txt"
+side_model_prod_path = Path(__file__).parent / "model" / "model_side_prod.txt"
 
 
 def get_side_model(is_livetrading: bool):
@@ -40,29 +41,37 @@ def get_side_model(is_livetrading: bool):
         return side_model
 
 
-# with open(Path(__file__).parent / "feature_info.json", "r") as f:
-#     feature_info = json.load(f)
+with open(Path(__file__).parent / "feature_info.json", "r") as f:
+    feature_info = json.load(f)
 
-# SIDE_DOLLAR_BAR_SHORT_FEATURES = feature_info["side"][DOLLAR_BAR_SHORT_TERM]
-# SIDE_DOLLAR_BAR_MID_FEATURES = feature_info["side"][DOLLAR_BAR_MID_TERM]
-# SIDE_DOLLAR_BAR_LONG_FEATURES = feature_info["side"][DOLLAR_BAR_LONG_TERM]
+SIDE_DOLLAR_BAR_SUPER_SHORT_FEATURES = feature_info["side"][DOLLAR_BAR_SUPER_SHORT_TERM]
+SIDE_DOLLAR_BAR_SHORT_FEATURES = feature_info["side"][DOLLAR_BAR_SHORT_TERM]
+SIDE_DOLLAR_BAR_MID_FEATURES = feature_info["side"][DOLLAR_BAR_MID_TERM]
+SIDE_DOLLAR_BAR_LONG_FEATURES = feature_info["side"][DOLLAR_BAR_LONG_TERM]
+SIDE_DOLLAR_BAR_SUPER_LONG_FEATURES = feature_info["side"][DOLLAR_BAR_SUPER_LONG_TERM]
 
-# SIDE_DOLLAR_BAR_ALL = (
-#     SIDE_DOLLAR_BAR_SHORT_FEATURES
-#     + SIDE_DOLLAR_BAR_MID_FEATURES
-#     + SIDE_DOLLAR_BAR_LONG_FEATURES
-# )
-# SIDE_ALL = SIDE_DOLLAR_BAR_ALL
+SIDE_DOLLAR_BAR_ALL = (
+    SIDE_DOLLAR_BAR_SUPER_SHORT_FEATURES
+    + SIDE_DOLLAR_BAR_SHORT_FEATURES
+    + SIDE_DOLLAR_BAR_MID_FEATURES
+    + SIDE_DOLLAR_BAR_LONG_FEATURES
+    + SIDE_DOLLAR_BAR_SUPER_LONG_FEATURES
+)
+SIDE_ALL = SIDE_DOLLAR_BAR_ALL
 
-# META_DOLLAR_BAR_SHORT_FEATURES = feature_info["meta"][DOLLAR_BAR_SHORT_TERM]
-# META_DOLLAR_BAR_MID_FEATURES = feature_info["meta"][DOLLAR_BAR_MID_TERM]
-# META_DOLLAR_BAR_LONG_FEATURES = feature_info["meta"][DOLLAR_BAR_LONG_TERM]
-# META_MODEL_SIDE_RES = feature_info["meta"]["model_res"]
+META_DOLLAR_BAR_SUPER_SHORT_FEATURES = feature_info["meta"][DOLLAR_BAR_SUPER_SHORT_TERM]
+META_DOLLAR_BAR_SHORT_FEATURES = feature_info["meta"][DOLLAR_BAR_SHORT_TERM]
+META_DOLLAR_BAR_MID_FEATURES = feature_info["meta"][DOLLAR_BAR_MID_TERM]
+META_DOLLAR_BAR_LONG_FEATURES = feature_info["meta"][DOLLAR_BAR_LONG_TERM]
+META_DOLLAR_BAR_SUPER_LONG_FEATURES = feature_info["meta"][DOLLAR_BAR_SUPER_LONG_TERM]
+META_MODEL_SIDE_RES = feature_info["meta"]["model_res"]
 
 
-# META_DOLLAR_BAR_ALL = (
-#     META_DOLLAR_BAR_SHORT_FEATURES
-#     + META_DOLLAR_BAR_MID_FEATURES
-#     + META_DOLLAR_BAR_LONG_FEATURES
-# )
-# META_ALL = META_DOLLAR_BAR_ALL + META_MODEL_SIDE_RES
+META_DOLLAR_BAR_ALL = (
+    META_DOLLAR_BAR_SUPER_SHORT_FEATURES
+    + META_DOLLAR_BAR_SHORT_FEATURES
+    + META_DOLLAR_BAR_MID_FEATURES
+    + META_DOLLAR_BAR_LONG_FEATURES
+    + META_DOLLAR_BAR_SUPER_LONG_FEATURES
+)
+META_ALL = META_DOLLAR_BAR_ALL + META_MODEL_SIDE_RES
