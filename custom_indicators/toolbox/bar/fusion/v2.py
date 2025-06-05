94a1bc1c83df25f73c8187b7e6b9d49f0ec31b5b
QIUYU YANG <wwwyqy618@gmail.com>
2025-06-05 23:34:46 +0800

    update fusion bar container

--- custom_indicators/toolbox/bar/fusion/v2.py
+++ custom_indicators/toolbox/bar/fusion/v2.py
@@ -10,45 +10,46 @@ from custom_indicators.toolbox.entropy.apen_sampen import sample_entropy_numba
 from custom_indicators.utils.math_tools import log_ret
 
 
-class FusionBarContainerV1(FusionBarContainerBase):
+class FusionBarContainerV2(FusionBarContainerBase):
     """
     Only valid for BTC futures
     """
 
-    def __init__(self, max_bars: int = 1000, threshold: float = 0.8548507667918396):
+    def __init__(self, max_bars: int = 1000, threshold: float = 1.3334030570418345):
         super().__init__(max_bars, threshold)
 
-        self.SHORT_N = 21
-        self.LONG_N = 180
-        self.ENTROPY_N = 34
+        self.N_1 = 200
+        self.N_2 = 121
+        self.ENTROPY_N = 72
 
     @property
     def max_lookback(self) -> int:
-        return self.LONG_N
+        return self.N_1
 
     def get_thresholds(self, candles: np.ndarray) -> np.ndarray:
         """
-        min(abs(r21), add(r180, r34_entropy))
+        min(abs(r200), add(r121, r72_entropy))
         """
-        log_ret_short_n = np.log(
-            candles[self.SHORT_N :, 2] / candles[: -self.SHORT_N, 2]
-        )[self.LONG_N - self.SHORT_N :]
+        log_ret_n_1 = np.log(candles[self.N_1 :, 2] / candles[: -self.N_1, 2])
 
-        log_ret_long_n = np.log(candles[self.LONG_N :, 2] / candles[: -self.LONG_N, 2])
+        log_ret_n_2 = np.log(candles[self.N_2 :, 2] / candles[: -self.N_2, 2])[
+            self.N_1 - self.N_2 :
+        ]
+
         entropy_log_ret_list = log_ret(
-            candles[self.LONG_N - self.ENTROPY_N :, :], self.ENTROPY_N
+            candles[self.N_1 - self.ENTROPY_N :, :], self.ENTROPY_N
         )
 
         with WorkerPool() as pool:
             entropy_array = pool.map(sample_entropy_numba, entropy_log_ret_list)
 
-        return np.min([np.abs(log_ret_short_n), log_ret_long_n + entropy_array], axis=0)
+        return np.min([np.abs(log_ret_n_1), log_ret_n_2 + entropy_array], axis=0)
 
 
 if __name__ == "__main__":
     candles = np.load("data/btc_1m.npy")
 
-    container = FusionBarContainerV1()
+    container = FusionBarContainerV2()
     # init
     container.update_with_candles(candles[:-10000])
     # update
