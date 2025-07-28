4193cec56d59b10b926579821423a87b567573ce
qiuyu.yang <qiuyu.yang@realai.ai>
2025-07-28 21:44:08 +0800

    deap v1 update

--- bar/tuning/deap_v1.py
+++ bar/tuning/deap_v1.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from bar.fusion.base import FusionBarContainerBase
 
 
@@ -10,7 +11,7 @@ class DeapBarV1(FusionBarContainerBase):
         vol_1,
         min(
           price_position,
-          vol_2
+          vol_1
         )
       ),
       min(
@@ -26,7 +27,6 @@ class DeapBarV1(FusionBarContainerBase):
     def __init__(
         self,
         VOL_1: int,
-        VOL_2: int,
         R_1: int,
         R_2: int,
         threshold: float,
@@ -34,13 +34,12 @@ class DeapBarV1(FusionBarContainerBase):
     ):
         super().__init__(max_bars, threshold)
         self.VOL_1 = VOL_1
-        self.VOL_2 = VOL_2
         self.R_1 = R_1
         self.R_2 = R_2
 
     @property
     def max_lookback(self) -> int:
-        return max(self.VOL_1, self.VOL_2, self.R_1, self.R_2)
+        return max(self.VOL_1, self.R_1, self.R_2)
 
     def get_thresholds(self, candles: np.ndarray) -> np.ndarray:
         close_arr = candles[:, 2]
@@ -53,9 +52,6 @@ class DeapBarV1(FusionBarContainerBase):
         log_vol_1 = np.log(vol_arr[self.VOL_1 :] / vol_arr[: -self.VOL_1])
         if self.max_lookback > self.VOL_1:
             log_vol_1 = log_vol_1[self.max_lookback - self.VOL_1 :]
-        log_vol_2 = np.log(vol_arr[self.VOL_2 :] / vol_arr[: -self.VOL_2])
-        if self.max_lookback > self.VOL_2:
-            log_vol_2 = log_vol_2[self.max_lookback - self.VOL_2 :]
         log_ret_1 = np.log(close_arr[self.R_1 :] / close_arr[: -self.R_1])
         if self.max_lookback > self.R_1:
             log_ret_1 = log_ret_1[self.max_lookback - self.R_1 :]
@@ -65,7 +61,7 @@ class DeapBarV1(FusionBarContainerBase):
 
         return np.min(
             [
-                log_vol_1 - np.min([price_pos, log_vol_2], axis=0),
+                log_vol_1 - np.min([price_pos, log_vol_1], axis=0),
                 np.min(
                     [np.min([price_pos, np.abs(log_ret_1)], axis=0), np.abs(log_ret_2)],
                     axis=0,
