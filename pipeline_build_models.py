313dec90a8a6e4ac712e166224bbe06ac7e41e95
QIUYU YANG <wwwyqy618@gmail.com>
2025-09-24 21:06:37 +0800

    models need retrain

--- pipeline_build_models.py
+++ pipeline_build_models.py
@@ -74,11 +74,11 @@ def build_model(lag: int, pred_next: int, is_regression: bool = False):
     feature_names = feature_selector.select_features(train_x, train_y)
     feature_selector.deep_ssm_model.save(MODEL_DEEP_SSM_PATH.resolve().as_posix())
     feature_selector.lg_ssm_model.save(MODEL_LG_SSM_PATH.resolve().as_posix())
-    with open(MODEL_DIR / "feature_info.json", "r") as f_r:
+    with open(feature_info_path, "r") as f_r:
         feature_info = json.load(f_r)
         feature_info[f"L{lag}_N{pred_next}"] = feature_names
 
-    with open(MODEL_DIR / "feature_info.json", "w") as f_w:
+    with open(feature_info_path, "w") as f_w:
         json.dump(feature_info, f_w, indent=4)
 
     model = lgb.train(best_model_param, lgb.Dataset(train_x, train_y))
